4a887d3a5162bd3b20ef0586c449fee768ba7574
Joseph Robinson <smartech56@gmail.com>
2019-01-29 17:43:48 -0700

    finished creating buildenv

--- scripts/buildenv.sh
+++ scripts/buildenv.sh
@@ -1,2 +1,23 @@
 # Make env folder and enter env folder
 mkdir env && cd env
+
+# Make 1.12.2 and 1.13.2 server folders
+mkdir 1.12.2
+mkdir 1.13.2
+
+# Get 1.13.2 files
+cd 1.13.2
+wget https://papermc.io/ci/job/Paper-1.13/lastSuccessfulBuild/artifact/paperclip.jar
+
+# go back to env folder
+cd ..
+
+# Get 1.12.2 files
+cd 1.12.2
+wget https://papermc.io/ci/job/Paper/lastSuccessfulBuild/artifact/paperclip.jar
+
+# go back to env folder
+cd ..
+
+# List files in env
+tree
