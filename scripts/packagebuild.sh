f0c2ee8fbb113be703f38ef3d1e3dac899613c52
Joseph Robinson <smartech56@gmail.com>
2019-01-29 19:23:22 -0700

    fixed issue with zipping files

--- scripts/packagebuild.sh
+++ scripts/packagebuild.sh
@@ -3,5 +3,5 @@ tree
 
 # zip 1.12.2 and 1.13.2 files for jenkins artifact
 cd env
-zip 1.12.2-server.zip 1.12.2/
-zip 1.13.2-server.zip 1.13.2/
+zip 1.12.2-server.zip 1.12.2/*
+zip 1.13.2-server.zip 1.13.2/*
